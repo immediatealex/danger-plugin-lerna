e676f6f9380919d4936fff48c05e71d0beec463d
Alex Mendes <alexanderhmendes@gmail.com>
2020-10-19 03:37:34 +0100

    feat: add basic settings

--- README.md
+++ README.md
@@ -21,3 +21,19 @@ import lerna from 'danger-plugin-lerna';
 
 schedule(lerna());
 ```
+
+If package changes are detected messages will be published like so:
+
+|        | Message                                                               |
+|--------|-----------------------------------------------------------------------|
+| :book: | :rocket: A new version of the `stuff` package will be published.      |
+| :book: | :rocket: A new version of the `more-stuff` package will be published. |
+
+## Settings
+
+The function accepts a settings object with the following properties:
+
+| name              | description                                                 |
+|-------------------|-------------------------------------------------------------|
+| `emoji`           | An emoji to prepend to the success message                  |
+| `noPublishMessage`| An optional message to show if there is nothing to publish. |
--- src/plugin.js
+++ src/plugin.js
@@ -7,7 +7,10 @@ import { loadConfig } from './load-config';
 /**
  * Report the next version to be published using lerna.
  */
-export const lerna = async () => {
+export const lerna = async ({
+  emoji = ':rocket',
+  noPublishMessage,
+}) => {
   const cwd = process.cwd();
   const pkgs = await getPackages(cwd);
   const graph = new PackageGraph(pkgs);
@@ -25,18 +28,10 @@ export const lerna = async () => {
   const changed = collectUpdates(pkgs, graph, { cwd }, lernaConfig);
 
   changed.forEach((pkg) => {
-    message(`:rocket: Merging this PR will publish a new version of the \`${pkg.name}\` package.`);
+    message(`${emoji} A new version of the \`${pkg.name}\` package will be published.`);
   });
 
-  if (!changed.length) {
-    message([
-      ':thinking: Merging this PR will not publish a new version of any packages.',
-      'If you want a new version of one of the packages in this mono-repo to be published please',
-      'add an appropriate semantic commit message (e.g. one prefixed with `fix` or `feat`)',
-      'when adding, modifying or deleting a file in that package. If you are not fixing a bug',
-      'or adding a new feature to a package perhaps you do not actually need anything published',
-      '(e.g. if fixing tests or adding a new dev dependency only).',
-      'See [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/) for more details.',
-    ].join(' '));
+  if (!changed.length && noPublishMessage) {
+    message(noPublishMessage);
   }
 };
